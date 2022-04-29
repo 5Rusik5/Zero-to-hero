57365ec35ac2cd30269ba54c974919a6d0ece700
yrysNM <yrys.n3m@gmail.com>
2022-04-29 08:43:44 +0600

    Merged all your features but !captcha doesn't work!

--- iswork/settings.py
+++ iswork/settings.py
@@ -40,8 +40,16 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'channels',
-    "room"
+    "room",
+    'django.contrib.humanize',
+    "hitcount",
+    "ckeditor", 
+    "captcha",
 ]
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.app_directories.load_template_source',
+)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -153,3 +161,9 @@ EMAIL_HOST_PASSWORD = "Himia.math.$.atom.3"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+
+RECAPTCHA_USE_SSL = False
+RECAPTCHA_PUBLIC_KEY = 'public'
+RECAPTCHA_PRIVATE_KEY = 'private'
+RECAPTCHA_OPTIONS = {'theme': 'white'}
